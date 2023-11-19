525eec137b799cbb3a79c3ab59942c1989d1d0d5
ThanhHung0203 <hungat418@gmail.com>
2023-11-19 20:39:21 +0700

    pull

--- Program.cs
+++ Program.cs
@@ -17,7 +17,6 @@ namespace DBMS_Project
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
-
         }
     }
 }
